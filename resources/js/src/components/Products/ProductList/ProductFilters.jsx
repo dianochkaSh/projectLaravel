6428ac700dcb967735f69a9810258d06ae319e71
Diana Shevchenko <d.shevchenko@inostudio.com>
2019-12-20 13:11:50 +0300

    filters in product lists

--- resources/js/src/components/Products/ProductList/ProductFilters.jsx
+++ resources/js/src/components/Products/ProductList/ProductFilters.jsx
@@ -1,11 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { observable } from 'mobx';
 
-const ProductFilters = () => {
+/* components */
+import InputField from '../../FormElements/InputField/InputField';
 
-    return (
-        <div>
-            <h4>Filters</h4>
-        </div>
-    )
+
+class ProductFilters extends Component {
+    @observable filters = {
+      priceStart: null,
+      priceEnd: null,
+    };
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div className="container-filters">
+                <h4>Filters</h4>
+                <div className="content-price">
+                    <label>Price:</label>
+                    <InputField
+                        typeFiled='text'
+                        nameField='priceStart'
+                        titleField=''
+                        IdInput='priceStart'
+                        valueField={this.priceStart}
+                        handlerFiled={this.handlerFieldValue}
+                    />
+                    <InputField
+                        typeFiled='text'
+                        nameField='priceEnd'
+                        titleField=''
+                        IdInput='priceEnd'
+                        valueField={this.priceEnd}
+                        handlerFiled={this.handlerFieldValue}
+                    />
+                </div>
+                <div>
+                    <label>Categories:</label>
+                </div>
+
+                <div>Author:</div>
+                <div>
+                    <button className="btn btn-primary" >Apply filters</button>
+                </div>
+            </div>
+        )
+    }
 };
 export default ProductFilters;
